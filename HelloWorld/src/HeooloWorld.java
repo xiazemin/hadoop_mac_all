506913454717ab8ac72ef84170aae2ba2b27cd42
夏泽民 <xiazemin@didichuxing.com>
2017-08-07 14:51:07 +0800

    hadoop

---
+++ HelloWorld/src/HeooloWorld.java
@@ -0,0 +1,7 @@
+import javax.print.attribute.standard.PrinterMessageFromOperator;
+
+public class HeooloWorld {
+ public static void main(String [] args){
+ System.out.println("hello world!");
+ }
+}
